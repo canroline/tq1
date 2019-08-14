84bd99072314f4db9f94f35d60f7d662ad6eddfe
canroline <zhoucuicui456@126.com>
2019-08-15 01:25:07 +0800

    model
    model

--- pages/tabBar/component/modelPage/modelPage.vue
+++ pages/tabBar/component/modelPage/modelPage.vue
@@ -3,24 +3,30 @@
 		 <!-- <model-obj :src="model_path"></model-obj> -->
 		 <!-- https://threejs.org/examples/#webgl_loader_obj2 -->
 		 <!-- https://uniapp.dcloud.io/static/web-view.html -->
-		 <!-- <web-view :webview-styles="webviewStyles" src="http://localhost:8080/#/manageUsers/userFund"></web-view> -->
+		 <web-view :webview-styles="webviewStyles" 
+		 src="https://feiwuar.goho.co/uploadFiles/threejs/box-spin.html"></web-view>
 	</view>
-</template>
+</template>  
 
 <!-- <script src="https://unpkg.com/vue-3d-model/dist/vue-3d-model.umd.js"></script> -->
 
 <script>
 	// import { ModelObj } from '@/components/vue-3D/vue-3d-model';
-	import { ModelObj } from 'vue-3d-model';
+	// import { ModelObj } from 'vue-3d-model';
 	// import package from 'packageName'
-	const ModelObj1 = require('vue-3d-model')
+	// const ModelObj1 = require('vue-3d-model')
 	export default {
 		components: {
 			// ModelObj
 		},
 		data() {
 			return {
-				model_path: 'static/model/LeePerrySmith.obj',
+				webviewStyles: {
+                    progress: {
+                        color: '#FF3333'
+                    }
+                }
+				// model_path: 'static/model/LeePerrySmith.obj',
 			}
 		},
 		onShow() {
