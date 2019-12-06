cccf400d675954e44830b2c3a0ba17ed04982cb3
caroline <>
2019-12-06 22:13:20 +0800

    添加媒体

--- pages/tabBar/component/component.vue
+++ pages/tabBar/component/component.vue
@@ -15,6 +15,9 @@
 		<view class="uni-flex uni-column" 
 			style="justify-content: center; align-items: center; height: 100%;padding: 20px;margin-top: -120px;">
 			
+			<video id="myVideo" src="https://dcloud-img.oss-cn-hangzhou.aliyuncs.com/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
+			                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+			
 			<button class="primary-btn" @click="goAR"
 				style="letter-spacing: 2px; padding: 10px;width: 80%; font-size: 20px;
 				background: linear-gradient(to right, #3A7CF6 , #55A7F8);" >
